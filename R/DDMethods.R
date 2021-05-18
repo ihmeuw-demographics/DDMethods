ac92d72e54c8d4027aec76e06809a3a54ff245ec
Katherine Paulson <krpaul@uw.edu>
2021-05-18 13:36:58 -0700

    initial setup from template

---
+++ R/DDMethods.R
@@ -0,0 +1,10 @@
+#' @title Death Distribution Methods
+#'
+#' @description Death Distribution Methods (DDMs) for the estimation of
+#'  completeness of death registration systems
+#'
+#' @docType package
+#' @name DDMethods
+#'
+#' @importFrom utils globalVariables
+NULL
